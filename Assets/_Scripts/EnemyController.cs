db815716c102b39b37f3492e50bf7f1fbb12e7c0
George Zhou <georgezhou89@hotmail.com>
2019-10-12 18:34:53 -0400

    Part 1 Complete

---
+++ Assets/_Scripts/EnemyController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Util;
+
+public class EnemyController : MonoBehaviour
+{
+    public EnemyAnimState enemyAnimState;
+
+    public Animator enemyAnmator;
+    public SpriteRenderer enemySpriteRenderer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        enemyAnimState = EnemyAnimState.WALK;    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Assets/_Scripts/PlayerController.cs
+++ Assets/_Scripts/PlayerController.cs
@@ -1,18 +1,52 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Util;
 
 public class PlayerController : MonoBehaviour
 {
+    public HeroAnimState heroAnimState;
+
+    public Animator heroAnimator;
+
+    public SpriteRenderer heroSpriteRenderer;
     // Start is called before the first frame update
     void Start()
     {
-        
+        heroAnimState = HeroAnimState.IDLE;
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //Idle
+        if (Input.GetAxis("Horizontal") == 0)
+        {
+            heroAnimState = HeroAnimState.IDLE;
+            heroAnimator.SetInteger("AnimState", (int)HeroAnimState.IDLE);
+        }
+        //Move Right
+        if (Input.GetAxis("Horizontal")>0)
+        {
+            heroAnimState = HeroAnimState.WALK;
+            heroAnimator.SetInteger("AnimState", (int)HeroAnimState.WALK);
+            heroSpriteRenderer.flipX = false;
+        }
+
+        //Move Left
+        if (Input.GetAxis("Horizontal") < 0)
+        {
+            heroAnimState = HeroAnimState.WALK;
+            heroAnimator.SetInteger("AnimState", (int)HeroAnimState.WALK);
+            heroSpriteRenderer.flipX = true;
+        }
+
+        //Jump
+        if (Input.GetAxis("Jump") > 0)
+        {
+            heroAnimState = HeroAnimState.JUMP;
+            heroAnimator.SetInteger("AnimState", (int)HeroAnimState.JUMP);
+        }
     }
 }
