de1f7fd82f5010f8799345e5603bd5bba1d2f928
George Zhou <georgezhou89@hotmail.com>
2019-10-14 13:27:21 -0400

    Updated Jump, Juimp Sound, and Camera Follow

--- Assets/_Scripts/PlayerController.cs
+++ Assets/_Scripts/PlayerController.cs
@@ -7,19 +7,38 @@ public class PlayerController : MonoBehaviour
 {
     public HeroAnimState heroAnimState;
 
+    [Header("Object Properties")]
     public Animator heroAnimator;
-
     public SpriteRenderer heroSpriteRenderer;
+    public Rigidbody2D heroRigidBody;
+
+    [Header("Physics Related")]
+    public float moveForce;
+    public float jumpForce;
+
+    public bool isGrounded;
+    public Transform groundTarget;
+
+    public AudioSource jumpSound;
+
     // Start is called before the first frame update
     void Start()
     {
         heroAnimState = HeroAnimState.IDLE;
-
+        isGrounded = false;
     }
 
     // Update is called once per frame
     void Update()
     {
+        isGrounded = Physics2D.Linecast(
+            transform.position, groundTarget.position,
+            1 << LayerMask.NameToLayer("Ground"));
+
+        isGrounded = Physics2D.BoxCast(
+            transform.position, new Vector2(2.0f, 1.0f), 0.0f, Vector2.down, 1.0f,
+            1 << LayerMask.NameToLayer("Ground"));
+
         //Idle
         if (Input.GetAxis("Horizontal") == 0)
         {
@@ -29,24 +48,35 @@ public class PlayerController : MonoBehaviour
         //Move Right
         if (Input.GetAxis("Horizontal")>0)
         {
-            heroAnimState = HeroAnimState.WALK;
-            heroAnimator.SetInteger("AnimState", (int)HeroAnimState.WALK);
             heroSpriteRenderer.flipX = false;
+            if (isGrounded)
+            {
+                heroAnimState = HeroAnimState.WALK;
+                heroAnimator.SetInteger("AnimState", (int)HeroAnimState.WALK);
+                heroRigidBody.AddForce(Vector2.right * moveForce);
+            }
         }
 
         //Move Left
         if (Input.GetAxis("Horizontal") < 0)
         {
-            heroAnimState = HeroAnimState.WALK;
-            heroAnimator.SetInteger("AnimState", (int)HeroAnimState.WALK);
             heroSpriteRenderer.flipX = true;
+            if (isGrounded)
+            {
+                heroAnimState = HeroAnimState.WALK;
+                heroAnimator.SetInteger("AnimState", (int)HeroAnimState.WALK);
+                heroRigidBody.AddForce(Vector2.left * moveForce);
+            }
         }
 
         //Jump
-        if (Input.GetAxis("Jump") > 0)
+        if ((Input.GetAxis("Jump") > 0) && (isGrounded))
         {
             heroAnimState = HeroAnimState.JUMP;
             heroAnimator.SetInteger("AnimState", (int)HeroAnimState.JUMP);
+            heroRigidBody.AddForce(Vector2.up * jumpForce);
+            isGrounded = false;
+            jumpSound.Play();
         }
     }
 }
